73eba4ccbc87a469a8e1420883003cec6f9b7e74
anntapiador <anntapiador@gmail.com>
2023-09-14 19:58:25 +1000

    Added code to connect to the database, fetch the values, and populate some Python lists with the corresponding values

--- climate_q1.py
+++ climate_q1.py
@@ -1,8 +1,14 @@
+import sqlite3
 import matplotlib.pyplot as plt
-        
-years = []
-co2 = []
-temp = []
+
+connection = sqlite3.connect(r"C:\Users\ajtor\OneDrive - Griffith University\04. Software Technologies\Quiz\Quiz_week_8\climate.db")
+cursor = connection.cursor()
+cursor.execute("SELECT* FROM ClimateData")
+result = cursor.fetchall()
+
+years = [row[0] for row in result]
+co2 = [row[1] for row in result]
+temp = [row[2] for row in result]
 
 plt.subplot(2, 1, 1)
 plt.plot(years, co2, 'b--') 
@@ -16,3 +22,5 @@ plt.ylabel("Temp (C)")
 plt.xlabel("Year (decade)") 
 plt.show() 
 plt.savefig("co2_temp_1.png") 
+
+connection.close()
